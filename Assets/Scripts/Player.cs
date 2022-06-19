51d80ff298e2b5a08a2d6e6acc1f2a3aa8b9ad0d
progdruid <progdruid@gmail.com>
2022-06-19 22:55:30 +0300

    Character, animation, and covering holes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,16 @@ public class Player : MonoBehaviour
     public float JumpCooldown;
     [Space]
     public float BBound;
+    public float FallingTimeThreshold;
 
     private float acc => MaxSpeed / AccTime;
     private float dec => MaxSpeed / DecTime;
 
     private Rigidbody2D rb;
-    private CircleCollider2D collider;
+    private new Collider2D collider;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private float fallingTime = 0f;
     private bool staying = false;
     private bool touchesLeft = false;
     private bool touchesRight = false;
@@ -26,8 +30,10 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        collider = gameObject.GetComponent<CircleCollider2D>();
+        collider = gameObject.GetComponent<Collider2D>();
         contactPoints = new List<ContactPoint2D>();
     }
 
@@ -38,7 +44,6 @@ public class Player : MonoBehaviour
 
         float horvalue = Input.GetAxisRaw("Horizontal");
         bool wallFree = !(touchesLeft && horvalue < 0f) && !(touchesRight && horvalue > 0f);
-
 
         if (horvalue != 0f && wallFree)  //acceleration
         {
@@ -59,10 +64,18 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(newvel, rb.velocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && staying) //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && fallingTime < FallingTimeThreshold) //Jump //Here is an exploit
+        {
             rb.velocity += Vector2.up * JumpForce;
+            staying = false;
+        }
 
-        
+        spriteRenderer.flipX = rb.velocity.x != 0f ? rb.velocity.x < 0f : spriteRenderer.flipX;
+
+        animator.SetBool("Falling", fallingTime >= FallingTimeThreshold);
+        animator.SetBool("Running", rb.velocity.x != 0f && fallingTime < FallingTimeThreshold);
+        if (fallingTime >= FallingTimeThreshold)
+            Debug.Log(fallingTime);
     }
 
     public void CheckForCollision (List<ContactPoint2D> contacts)
@@ -70,20 +83,27 @@ public class Player : MonoBehaviour
         bool oneTouchesLeft = false;
         bool oneTouchesRight = false;
         staying = false;
+        bool falls = true;
 
         foreach (var contact in contacts)
         {
             Vector2 point = contact.point - (Vector2)gameObject.transform.position;
             point.Normalize();
 
-            if (point.y <= -BBound && !staying) //Checking bottom
+            if (point.y <= -BBound) //Checking bottom
+            {
                 staying = true;
+                falls = false;
+                fallingTime = 0f;
+            }
 
             if (!oneTouchesLeft && point.x < 0f)                //Checking left side
                 oneTouchesLeft = Mathf.Abs(point.y) < BBound;
             else if (!oneTouchesRight && point.x > 0f)          //Checking right side
                 oneTouchesRight = Mathf.Abs(point.y) < BBound;
         }
+        if (falls)
+            fallingTime += Time.deltaTime;
 
         touchesLeft = oneTouchesLeft;
         touchesRight = oneTouchesRight;
