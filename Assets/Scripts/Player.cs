769703da974aeab2f0a2f6d387f4b9915d1f458d
progdruid <progdruid@gmail.com>
2022-06-19 17:47:41 +0300

    movement is even more bellissimo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,64 +4,82 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float SpeedOnGround;
-    public float SpeedInAir;
+    public float MaxSpeed;
+    public float AccTime;
+    public float DecTime;
+    [Space]
     public float JumpForce;
+    public float JumpCooldown;
     [Space]
     public float BBound;
-    public float TimeForMovement;
-    public float TimeForJump;
+
+    private float acc => MaxSpeed / AccTime;
+    private float dec => MaxSpeed / DecTime;
 
     private Rigidbody2D rb;
-    private float timeStaying = 0f;
+    private CircleCollider2D collider;
+    private bool staying = false;
     private bool touchesLeft = false;
     private bool touchesRight = false;
+
+    private List<ContactPoint2D> contactPoints;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<CircleCollider2D>();
+        contactPoints = new List<ContactPoint2D>();
     }
 
     void Update()
     {
-        float vel = Input.GetAxis("Horizontal");
+        collider.GetContacts(contactPoints);
+        CheckForCollision(contactPoints);
 
-        if (timeStaying > TimeForMovement)
-            vel *= SpeedOnGround;
-        else
-            vel *= SpeedInAir;
+        float horvalue = Input.GetAxisRaw("Horizontal");
+        bool wallFree = !(touchesLeft && horvalue < 0f) && !(touchesRight && horvalue > 0f);
 
-        if (vel != 0f && !(touchesLeft && vel < 0f) && !(touchesRight && vel > 0f))
-            rb.velocity = new Vector2(vel, rb.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && timeStaying >= TimeForJump)
+        if (horvalue != 0f && wallFree)  //acceleration
         {
-            rb.velocity += Vector2.up * JumpForce;
-            timeStaying = 0f;
+            float newvel = rb.velocity.x + acc * Time.deltaTime * horvalue;
+
+            if (Mathf.Abs(newvel) > MaxSpeed)
+                newvel = MaxSpeed * Mathf.Sign(newvel);
+
+            rb.velocity = new Vector2(newvel, rb.velocity.y);
         }
+        else if (horvalue == 0f && rb.velocity.x != 0f) //deceleration
+        {
+            float sign = Mathf.Sign(rb.velocity.x);
+            float newvel = rb.velocity.x - dec * Time.deltaTime * sign;
+            if (newvel * sign < 0f)
+                newvel = 0f;
+
+            rb.velocity = new Vector2(newvel, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && staying) //Jump
+            rb.velocity += Vector2.up * JumpForce;
+
+        
     }
 
-    public void OnCollisionStay2D(Collision2D collision)
+    public void CheckForCollision (List<ContactPoint2D> contacts)
     {
-        if (collision.gameObject.tag != "Ground")
-            return;
-
         bool oneTouchesLeft = false;
         bool oneTouchesRight = false;
+        staying = false;
 
-        bool groundChecked = false;
-
-        foreach (var contact in collision.contacts)
+        foreach (var contact in contacts)
         {
             Vector2 point = contact.point - (Vector2)gameObject.transform.position;
             point.Normalize();
 
-            if (point.y <= -BBound && timeStaying < 1 && !groundChecked) //Checking bottom
-            {
-                timeStaying += Time.deltaTime;
-                groundChecked = true;
-            }
-            else if (!oneTouchesLeft && point.x < 0f)           //Checking left side
+            if (point.y <= -BBound && !staying) //Checking bottom
+                staying = true;
+
+            if (!oneTouchesLeft && point.x < 0f)                //Checking left side
                 oneTouchesLeft = Mathf.Abs(point.y) < BBound;
             else if (!oneTouchesRight && point.x > 0f)          //Checking right side
                 oneTouchesRight = Mathf.Abs(point.y) < BBound;
