4681a3b5ab55bd6913856cb3e4d3223bad3d97cd
progdruid <progdruid@gmail.com>
2022-10-22 22:58:56 +0100

    locks

---
+++ Assets/Scripts/Props/Lock.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Lock : MonoBehaviour
+{
+    [SerializeField] SignalActivator signal;
+    [SerializeField] TileBase tile1, tile2;
+
+    private Tilemap tilemap;
+    private Collider2D col;
+
+    private void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        col = GetComponent<Collider2D>();
+
+        signal.ActivationUpdateEvent += HandleActivation;
+    }
+
+    private void OnDestroy()
+    {
+        signal.ActivationUpdateEvent -= HandleActivation;
+    }
+
+    private void HandleActivation (bool activated, GameObject source)
+    {
+        col.enabled = !activated;
+        if (activated)
+            tilemap.SwapTile(tile1, tile2);
+        else
+            tilemap.SwapTile(tile2, tile1);
+    }
+}
