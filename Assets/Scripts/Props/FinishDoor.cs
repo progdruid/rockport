24047b48f21c8afe6fc623b8535427a7ced942da
progdruid <progdruid@gmail.com>
2022-12-27 21:42:43 +0000

    finish door refactoring

--- Assets/Scripts/Props/FinishDoor.cs
+++ Assets/Scripts/Props/FinishDoor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishTriggerHandler : MonoBehaviour
+public class FinishDoor : MonoBehaviour
 {
     public int LoadLevelIndex;
 
