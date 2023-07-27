b4c0e7566bf45bdd9239ddd67d899a3045187a1b
progdruid <progdruid@gmail.com>
2023-07-27 23:10:05 +0100

    new fruit graphics implemented

---
+++ Assets/Scripts/Props/Fruit.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UniversalTrigger))]
+public class Fruit : MonoBehaviour
+{
+    private UniversalTrigger _trigger;
+    private Animator _animator;
+
+    #region ceremony
+    private void Start()
+    {
+        _trigger = GetComponent<UniversalTrigger>();
+        _animator = GetComponent<Animator>();
+        _trigger.EnterEvent += HandleTriggerEnter;
+    }
+
+    private void OnDestroy()
+    {
+        _trigger.EnterEvent -= HandleTriggerEnter;
+    }
+    #endregion
+
+    private void HandleTriggerEnter(Collider2D other, TriggeredType type)
+    {
+        if (type == TriggeredType.Player)
+            StartCoroutine(Collect());
+    }
+
+    private IEnumerator Collect ()
+    {
+        _animator.SetTrigger("Collect");
+        Registry.ins.skullManager.AddSkull();
+        yield return new WaitUntil(() => _animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "None");
+        Destroy(gameObject);
+    }
+}
