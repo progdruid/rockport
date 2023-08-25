1cb3f1c7e405d5cb8c36c150f4dc9d0917cded1f
progdruid <progdruid@gmail.com>
2023-08-25 19:59:37 +0100

    parallax unificated and implemented in menu

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] [Range(0f, 1f)] float parallaxCoeficient;
-    
+    [SerializeField] Sprite sampleSprite;
+    [SerializeField] Vector2 cycleMovementSpeed;
+
     private LevelLoader levelLoader;
     private Vector2 equatorialLinePoint;
     private Transform targetTransform;
+    private float halfWidth;
 
     private void Awake()
     {
-        levelLoader = GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelLoader>();
-        levelLoader.levelInstantiationEvent += Init;
+        GameObject gc = GameObject.FindGameObjectWithTag("GameController");
+        if (gc != null)
+            levelLoader = gc.GetComponent<LevelLoader>();
+        if (levelLoader != null)
+            levelLoader.levelInstantiationEvent += Init;
+
+        halfWidth = sampleSprite.bounds.extents.x;
+    }
+
+    private void Start()
+    {
+        if (levelLoader == null)
+            Init();
     }
 
     private void OnDestroy()
@@ -26,7 +40,11 @@ public class ParallaxBackground : MonoBehaviour
     {
         targetTransform = Camera.main.gameObject.transform;
 
-        GameObject foundObject = levelLoader.TryFindObjectWithTag("EquatorialLineObject");
+        GameObject foundObject;
+        if (levelLoader != null)
+            foundObject = levelLoader.TryFindObjectWithTag("EquatorialLineObject");
+        else
+            foundObject = GameObject.FindGameObjectWithTag("EquatorialLineObject");
 
         if (foundObject != null)
             equatorialLinePoint = (Vector2)foundObject.transform.position;
@@ -37,9 +55,15 @@ public class ParallaxBackground : MonoBehaviour
 
     void FixedUpdate() 
     {
-        transform.position = new Vector3(
-            equatorialLinePoint.x * (1f - parallaxCoeficient) + targetTransform.position.x * parallaxCoeficient,
-            equatorialLinePoint.y * (1f - parallaxCoeficient) + targetTransform.position.y * parallaxCoeficient,
-            transform.position.z);
+        float newx = equatorialLinePoint.x * (1f - parallaxCoeficient) + targetTransform.position.x * parallaxCoeficient;
+        float newy = equatorialLinePoint.y * (1f - parallaxCoeficient) + targetTransform.position.y * parallaxCoeficient;
+        newx += Time.time * cycleMovementSpeed.x * (1f - parallaxCoeficient);
+        newy += Time.time * cycleMovementSpeed.y * (1f - parallaxCoeficient);
+        
+        newx = Mod(newx - Camera.main.transform.position.x + halfWidth, halfWidth * 2) + Camera.main.transform.position.x - halfWidth;
+
+        transform.position = new Vector3(newx, newy, transform.position.z);
     }
+
+    private float Mod(float a, float b) => (a % b + b) % b;
 }
