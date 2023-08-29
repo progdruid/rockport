23f91b9eee99606d540c8ea469b61c0108de2883
progdruid <progdruid@gmail.com>
2023-08-29 15:23:58 +0100

    level list sliding

--- Assets/Scripts/UI/LevelListBehaviour.cs
+++ Assets/Scripts/UI/LevelListBehaviour.cs
@@ -5,75 +5,93 @@ using UnityEngine;
 public class LevelListBehaviour : MonoBehaviour
 {
     [SerializeField] int heightInLevels;
-    [SerializeField] float slideAnimationDuration;
+    [SerializeField] float acceleration;
+    [SerializeField] float oscillationAcc;
+    [SerializeField] float oscillationMinInitSpeed;
+    [SerializeField] float nearThreshold;
     [SerializeField] Sprite sampleUnitSprite;
 
     private float unitHeight;
     private float defaultY;
 
-    private int movingDir = 0;
-    private int currentUnit = 0;
+    private float speed = 0;
+    private int selectedUnit = 0;
+    private bool oscillating = false;
+    private bool oscillationDone = true;
+    private bool selectionChanged = false;
 
-    private Coroutine currentRoutine;
+    private float selectedY => defaultY - unitHeight * selectedUnit;
 
     public void MoveListUp ()
     {
-        Debug.Log(currentUnit);
-        if (movingDir == 0 && currentUnit > 0)
-            currentRoutine = StartCoroutine(MoveListRoutine(1));
-        else if (movingDir > 0 && currentUnit > 1)
-        {
-            currentUnit--;
-            transform.position += Vector3.up * unitHeight;
-        }
+        SetLevel(selectedUnit - 1);
     }
 
     public void MoveListDown ()
     {
-        if (movingDir == 0 && currentUnit < heightInLevels - 1)
-            currentRoutine = StartCoroutine(MoveListRoutine(-1));
-        else if (movingDir > 0 && currentUnit < heightInLevels - 2)
-        {
-            currentUnit++;
-            transform.position += Vector3.down * unitHeight;
-        }
+        SetLevel(selectedUnit + 1);
     }
 
     public void SetLevel (int levelIndex)
     {
-        if (currentRoutine != null)
-            StopCoroutine(currentRoutine);
-        movingDir = 0;
+        if (levelIndex >= 0 && levelIndex < heightInLevels)
+        {
+            selectionChanged = selectedUnit != levelIndex || selectionChanged; //make it true if selectedUnit != levelIndex
+            oscillationDone = selectedUnit == levelIndex && oscillationDone; //make it false if selectedUnit != levelIndex
 
-        currentUnit = levelIndex;
-        transform.position = new Vector3(transform.position.x, defaultY - currentUnit * unitHeight, transform.position.z);
+            selectedUnit = levelIndex;
+        }
     }
 
-    private IEnumerator MoveListRoutine (int dir)
+    private IEnumerator OscillationRoutine (float initSpeed)
     {
-        movingDir = dir;
-        float startTime = Time.time;
+        oscillating = true;
 
-        while (Time.time - startTime < slideAnimationDuration)
+        if (Mathf.Abs(initSpeed) < oscillationMinInitSpeed)
+            initSpeed = Mathf.Sign(initSpeed) * oscillationMinInitSpeed;
+        
+        float timeStarted = Time.time;
+        float oscillationDuration = Mathf.Abs(initSpeed) * 2f / oscillationAcc;
+        float accdir = -Mathf.Sign(initSpeed);
+        float initY = transform.position.y;
+
+        while (!selectionChanged && Time.time - timeStarted < oscillationDuration)
         {
-            float heightInUnitsAdd = InterpFunc((Time.time - startTime) / slideAnimationDuration) * dir;
-            transform.position = new Vector3(
-                transform.position.x, 
-                defaultY - (currentUnit - heightInUnitsAdd) * unitHeight, 
-                transform.position.z);
-            
+            float timePassed = Time.time - timeStarted;
+            float displacement = initSpeed * timePassed + oscillationAcc * accdir * timePassed * timePassed / 2f;
+            transform.position = new(transform.position.x, initY + displacement, transform.position.z);
             yield return new WaitForFixedUpdate();
         }
 
-        currentUnit -= dir;
-        transform.position = new Vector3(transform.position.x, defaultY - currentUnit * unitHeight, transform.position.z);
+        if (!selectionChanged)
+            transform.position = new(transform.position.x, initY, transform.position.z);
 
-        movingDir = 0;
-        currentRoutine = null;
+        oscillating = false;
     }
 
-    private float InterpFunc(float t) => t;
+    private void FixedUpdate()
+    {
+        if (oscillating)
+            return;
 
+        if (Mathf.Abs(selectedY - transform.position.y) <= nearThreshold && !oscillationDone)
+        {
+            transform.position = new(transform.position.x, selectedY, transform.position.z);
+            selectionChanged = false;
+            
+            if (speed != 0)
+                StartCoroutine(OscillationRoutine(speed));
+
+            oscillationDone = true;
+            speed = 0;
+        }
+        else if (Mathf.Abs(selectedY - transform.position.y) > nearThreshold)
+        {
+            int dir = (int)Mathf.Sign(selectedY - transform.position.y);
+            speed += acceleration * dir * Time.fixedDeltaTime;
+            transform.position += Vector3.up * speed * Time.fixedDeltaTime;
+        }
+    }
 
     void Start()
     {
